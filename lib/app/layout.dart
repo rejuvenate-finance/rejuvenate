cee9d5b1cde864d030e7afa67a1e7ebcb560d0e9
Kevin Scheeren <contact@kevinscheeren.de>
2022-08-01 18:14:22 +0200

    removed old data

--- lib/app/layout.dart
+++ lib/app/layout.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rejuvenate/app/theme.dart';
 import 'package:rejuvenate/widgets/connect_button.dart/connect_button.dart';
-import 'package:rejuvenate/widgets/rotated_navigation_bar/rotated_navigation_bar.dart';
 import 'package:rejuvenate/widgets/navigation_menu/navigation_menu.dart';
-import 'package:responsive_builder/responsive_builder.dart';
-
-import '../widgets/navigation_bar/navigation_bar.dart';
 
 class AppLayout extends StatelessWidget {
   const AppLayout({Key? key, required this.title, required this.child})
@@ -35,35 +31,6 @@ class AppLayout extends StatelessWidget {
       drawer: const NavigationMenu(),
       extendBodyBehindAppBar: false,
       body: child,
-    );
-  }
-}
-
-class HomeLayout extends StatelessWidget {
-  const HomeLayout({Key? key, required this.child}) : super(key: key);
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: AppTheme.dark.primaryColor,
-      ),
-      body: Stack(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: child,
-          ),
-        ],
-      ),
-      endDrawer: const Drawer(
-        width: 300.0,
-        child: RotatedNavigationBar(),
-      ),
     );
   }
 }
--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -3,12 +3,8 @@ import 'package:rejuvenate/data/team_member.dart';
 import 'package:rejuvenate/views/dashboard/dashboard_view.dart';
 import 'package:rejuvenate/views/farms/farms_view.dart';
 import 'package:rejuvenate/views/home/home_view.dart';
-import 'package:rejuvenate/views/otc/otc_view.dart';
 import 'package:rejuvenate/views/rug_recovery/rug_recovery_view.dart';
-import 'package:rejuvenate/views/services/services_view.dart';
 import 'package:rejuvenate/views/staking/staking_view.dart';
-import 'package:rejuvenate/views/team/team_view.dart';
-import 'package:rejuvenate/views/team_detail/team_deatil_view.dart';
 
 abstract class AppRouter {
   static Route<dynamic>? generateRoute(RouteSettings settings) =>
@@ -43,33 +39,6 @@ Route<dynamic>? _generateRoute(RouteSettings settings) {
         builder: (context) => const RugRecoveryView(),
         settings: settings,
       );
-    case AppRoutes.services:
-      return MaterialPageRoute(
-        builder: (context) => const ServicesView(),
-        settings: settings,
-      );
-    case AppRoutes.team:
-      return MaterialPageRoute(
-        builder: (context) => const TeamView(),
-        settings: settings,
-      );
-    case AppRoutes.otc:
-      return MaterialPageRoute(
-        builder: (context) => const OTCView(),
-        settings: settings,
-      );
-  }
-  if (settings.name!.contains(AppRoutes.team)) {
-    final ending = settings.name!.split("/").last;
-    final member = teamMembers.firstWhere(
-      (element) => element.name.toLowerCase() == ending.toLowerCase(),
-    );
-    return MaterialPageRoute(
-      builder: (context) => TeamDetailView(
-        teamMember: member,
-      ),
-      settings: settings,
-    );
   }
   return null;
 }
@@ -79,11 +48,8 @@ final _key = GlobalKey<NavigatorState>();
 //ROUTES
 abstract class AppRoutes {
   static const String home = "/";
-  static const String services = "/services";
-  static const String team = "/team";
   static const String dashboard = "/dashboard";
   static const String staking = "/staking";
   static const String farms = "/farms";
   static const String rugRecovery = "/rug-recovery";
-  static const String otc = "/otc";
 }
